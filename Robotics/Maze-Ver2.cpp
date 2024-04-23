674267edb6a767f7523ab101f9cc839dd9580cfa
Zalfa Nafila <92864261+zalvexe@users.noreply.github.com>
2024-04-23 10:29:19 +0700

    Update Maze-Ver2.cpp

--- Robotics/Maze-Ver2.cpp
+++ Robotics/Maze-Ver2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define pb push_back
 /* 
----- RIVAL ROBOTICS INTERNSHIP FIRST TASK ----
+---- RIVAL ROBOTICS INTERNSHIP FIRST TASK (OOP) ----
 PENJELASAN :
 1. grid maze disimpan di vector 2D, kenapa vektor? biar lebih mudah saat passing ke function 
 karena size n m tidak pasti (tergantung input)
